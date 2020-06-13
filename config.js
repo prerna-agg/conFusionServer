b653f727b61c1eb6eaf98751987854c7e4b92af3
prerna <paruprerna@gmail.com>
2020-06-13 22:25:59 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: '12345-67890-09876-54321',
 	mongoUrl: 'mongodb://localhost:27017/conFusion',
+	facebook: {
+		clientId: '819737998554111',
+		clientSecret: '9578498bbb52b6301b25e98cb3938c4d',
+	},
 };
